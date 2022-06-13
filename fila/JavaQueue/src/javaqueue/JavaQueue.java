b3983d4e4132bf7ddae7c22b413eb41d1ae2b099
João Miguel Vieira <103217121+JoaoMiguel2@users.noreply.github.com>
2022-06-13 17:20:02 -0300

    Add files via upload

---
+++ fila/JavaQueue/src/javaqueue/JavaQueue.java
@@ -0,0 +1,13 @@
+
+package javaqueue;
+
+
+public class JavaQueue {
+
+
+    public static void main(String[] args) {
+        FormFila form = new FormFila();
+        form.setVisible(true);
+    }
+    
+}
